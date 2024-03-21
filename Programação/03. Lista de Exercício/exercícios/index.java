6f92cefcbef2d16b59b61e7a8e148e73277a6143
Caio <ocaiomoises@gmail.com>
2024-03-21 13:55:02 -0300

    input dos pontos

--- Programação/03. Lista de Exercício/exercícios/index.java
+++ Programação/03. Lista de Exercício/exercícios/index.java
@@ -1,10 +1,21 @@
-import Java.util.Scanner
+import java.util.Scanner;
 
-public class index{
-    public static void main(args[], Str){
-        sc = new Scanner()
+public class index {
+    public static void main(String[] agrs) {
+        Scanner sc = new Scanner(System.in);
         int x1, y1, x2, y2, distancia;
-        System.out.print("Digite o ponto x1: ")
+        System.out.print("Digite o ponto x1: ");
+        x1 = sc.nextInt();
 
+        System.out.print("Digite o ponto y1: ");
+        y1 = sc.nextInt();
+
+        System.out.print("Digite o ponto x2: ");
+        x2 = sc.nextInt();
+
+        System.out.print("Digite o ponto y2: ");
+        y2 = sc.nextInt();
+
+        sc.close();
     }
 }
