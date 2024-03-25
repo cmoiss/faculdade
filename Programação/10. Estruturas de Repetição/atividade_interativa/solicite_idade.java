2cd191a1841a31a27b2579abfc671e40e093896c
Caio Moisés Souza Barbalho <160979584+cmoiss@users.noreply.github.com>
2024-03-25 11:28:20 -0300

    Solicita Idade

---
+++ Programação/10. Estruturas de Repetição/atividade_interativa/solicite_idade.java
@@ -0,0 +1,13 @@
+package atividade_interativa;
+import java.util.Scanner;
+
+public class solicite_idade {
+    public static void main(String args) {
+        Scanner Read = new Scanner(System.in);
+
+        System.out.print("Digite sua idade: ");
+        
+        
+        Read.close();
+    }
+}
