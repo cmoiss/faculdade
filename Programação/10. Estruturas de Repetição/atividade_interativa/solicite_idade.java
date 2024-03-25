1314ea5305807807456cc204e26cafe8cb77090a
Caio <ocaiomoises@gmail.com>
2024-03-25 13:05:51 -0300

    add age count

--- Programação/10. Estruturas de Repetição/atividade_interativa/solicite_idade.java
+++ Programação/10. Estruturas de Repetição/atividade_interativa/solicite_idade.java
@@ -1,14 +1,42 @@
 package atividade_interativa;
 import java.util.Scanner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class solicite_idade {
     public static void main(String args) {
         Scanner Read = new Scanner(System.in);
-        int idade;
+        int age;
+        List <Integer> numPeople = new ArrayList<>(); //quantidade de pessoas  
+        //int numPeople; //quantidade de pessoas
 
-        System.out.print("Digite sua idade: ");
-        idade = nextInt();
+        /*
+        System.out.print("Digite a quantidade de pessoas: ");
+        numPeople = Read.nextInt();
+        */
+
+        do {
+            System.out.println("Digite a idade: ");
+            age = Read.nextInt();
+
+            numPeople.add(age);
+        } while(age >= 0);
+
+/*
+        for(numPeople = 0; age < 0; numPeople++) {
+
+        }
+*/
+
+/*
+        while (age >= 0) {
+            System.out.println("Digite a idade: ");
+            age = Read.nextInt(); 
+        }
+*/
         
+
         Read.close();
     }
 }
