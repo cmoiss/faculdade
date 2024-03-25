de7821f42e72b6a30b7053efd8942adbcb1c8378
Caio Moisés Souza Barbalho <160979584+cmoiss@users.noreply.github.com>
2024-03-25 11:02:56 -0300

    atividade interativa

---
+++ Programação/10. Estruturas de Repetição/atividade_interativa/exibir_pares.java
@@ -0,0 +1,16 @@
+package atividade_interativa;
+import java.util.Scanner;
+
+public class exibir_pares {
+    public static void main(String[] args){
+        Scanner Read = new Scanner(System.in);
+        int number;
+
+        System.out.print("Digite um número: ");
+        number = Read.nextInt();
+
+        System.err.println(0);
+
+
+    }
+}
