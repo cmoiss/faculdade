205fcda156f8964438f54eb755f4f62f5247266d
Caio Moisés Souza Barbalho <160979584+cmoiss@users.noreply.github.com>
2024-03-25 10:33:02 -0300

    contador usando For

---
+++ Programação/10. Estruturas de Repetição/contador_for.java
@@ -0,0 +1,9 @@
+public class contador_for{
+    public static void main(String[] args){
+        int x;
+        
+        for(x=1;x<=100;x++){
+            System.out.println(x);
+        }
+    }
+}
