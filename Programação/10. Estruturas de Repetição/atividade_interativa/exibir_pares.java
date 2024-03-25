198927d39bed52968512fdcdbd3d31e2d4dd3338
Caio Moisés Souza Barbalho <160979584+cmoiss@users.noreply.github.com>
2024-03-25 11:17:16 -0300

    atividade 1

--- Programação/10. Estruturas de Repetição/atividade_interativa/exibir_pares.java
+++ Programação/10. Estruturas de Repetição/atividade_interativa/exibir_pares.java
@@ -4,13 +4,28 @@ import java.util.Scanner;
 public class exibir_pares {
     public static void main(String[] args){
         Scanner Read = new Scanner(System.in);
+        int i = 0;
         int number;
+        int resto;
 
         System.out.print("Digite um número: ");
         number = Read.nextInt();
 
-        System.err.println(0);
+//        System.out.println(0);
 
+        while(i <= number){
+            resto = i % 2;
+            if (resto == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
+
+/*
+        for(i=1; limit == number; i++){
+            
+        }
+*/
 
     }
 }
--- Programação/10. Estruturas de Repetição/contador_de_pares.java
+++ Programação/10. Estruturas de Repetição/contador_de_pares.java
@@ -3,6 +3,8 @@ public class contador_de_pares {
         int i = 1;
         int resto;
 
+//        i = i + 2;
+
         while (i <= 100) {
             resto = i % 2;
             if (resto == 0) {
