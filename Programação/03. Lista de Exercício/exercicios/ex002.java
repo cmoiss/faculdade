bf80a1d1ad2c03ea8d13090d144746d401623ca7
Caio Moisés Souza Barbalho <ocaiomoises@gmail.com>
2024-03-27 00:22:24 -0300

    do while

--- Programação/03. Lista de Exercício/exercicios/ex002.java
+++ Programação/03. Lista de Exercício/exercicios/ex002.java
@@ -8,16 +8,21 @@ public class ex002 {
         int a, b, c, r, s, d;
         Scanner Read = new Scanner(System.in);
 
-        System.out.print("Digite o valor de A: ");
-        a = Read.nextInt();
+        do {
+            System.out.print("Digite o valor de A, sendo ele um número positivo (>0): ");
+            a = Read.nextInt();
+        } while (a < 1);
+        
+        //ex001.espaço_branco();
+        do {
+            System.out.print("Digite o valor de B, sendo ele um número positivo (>0): ");
+            b = Read.nextInt();
+        } while (b < 1);
 
         //ex001.espaço_branco();
-        System.out.print("Digite o valor de B: ");
-        b = Read.nextInt();
-
-        System.out.print("Digite o valor de C: ");
-        c = Read.nextInt();
-
-
+        do {
+            System.out.print("Digite o valor de C, sendo ele um número positivo (>0): ");
+            c = Read.nextInt();
+        } while (c < 1);
     }
 }
