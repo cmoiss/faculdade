f62e9168488dbd4eb74c74b15ca73dd990dad3ad
Caio Moisés Souza Barbalho <ocaiomoises@gmail.com>
2024-03-26 23:57:47 -0300

    Removi os comentários de coisas antigas do código

--- Programação/03. Lista de Exercício/exercícios/ex001.java
+++ Programação/03. Lista de Exercício/exercícios/ex001.java
@@ -8,24 +8,18 @@ public class ex001 {
         int[] ponto_02 = new int[2];
         
         System.out.println("Digite as coordenadas do ponto 1 (x1, y1)");
-        //x1 = Ler.nextInt();
-        //y1 = Ler.nextInt();
 
         ponto_01[0] = Ler.nextInt();
         ponto_01[1] = Ler.nextInt();
         
         espaço_branco();
         System.out.println("Ótimo, agora digite as coordenadas do ponto 2 (x2, y2)");
-        //x2 = ponto_01[0];
-        //y2 = ponto_01[1];
         
         ponto_02[0] = Ler.nextInt();
         ponto_02[1] = Ler.nextInt();
 
         espaço_branco();
         calcular_distancia(ponto_01, ponto_02);
-
-        //System.out.print(ponto_01[0] + ponto_01[1]);
         
         Ler.close();
     }
@@ -49,33 +43,3 @@ public class ex001 {
         System.out.print("");
     }
 }
-
-
-
-/*
-import java.util.Scanner;
-
-public class ex001 {
-    public static String exercicio01() {
-        Scanner Ler = new Scanner(System.in);
-        int x1, y1, x2, y2, distancia;
-        System.out.print("Digite o ponto x1 de A: ");
-        x1 = Ler.nextInt();
-
-        System.out.print("Digite o ponto y1 de A: ");
-        y1 = Ler.nextInt();
-
-        System.out.print("Digite o ponto x2 de B: ");
-        x2 = Ler.nextInt();
-
-        System.out.print("Digite o ponto y2 de B: ");
-        y2 = Ler.nextInt();
-
-        distancia = (x2 - x1) + (y2 - y1);
-        System.out.print("A distância entre os pontos A e B é de: " + distancia);
-
-        Ler.close();
-        return null;
-    }
-}
-*/
