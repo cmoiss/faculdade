0cbf2d7c3d0ae63cbacc9dcbcda9d84455f245a9
Caio Moisés Souza Barbalho <ocaiomoises@gmail.com>
2024-03-26 23:27:02 -0300

    função calcular distância
    criei uma função para calcular a distância

--- Programação/03. Lista de Exercício/exercícios/ex001.java
+++ Programação/03. Lista de Exercício/exercícios/ex001.java
@@ -4,22 +4,33 @@ public class ex001 {
     public static void main(String[] args) {
         Scanner Ler = new Scanner(System.in);
         int x1, y1, x2, y2, distancia;
-        int[] ponto_01 = new int[2];
-        int[] ponto_02 = new int[2];
+        int ponto_01[] = new int[2];
+        int ponto_02[] = new int[2];
         
         System.out.println("Digite as coordenadas do ponto 1 (x1, y1)");
-        x1 = Ler.nextInt();
-        x2 = Ler.nextInt();
+        //x1 = Ler.nextInt();
+        //y1 = Ler.nextInt();
 
-        ponto_01[0] = x1;
-        ponto_01[1] = x2;
+        ponto_01[0] = Ler.nextInt();
+        ponto_01[1] = Ler.nextInt();
         
-        x1 = ponto_01[0];
-        //x2 = ponto_01[1];
+        System.out.println("Ótimo, agora digite as coordenadas do ponto 2 (x2, y2)");
+        //x2 = ponto_01[0];
+        //y2 = ponto_01[1];
         
-        System.out.print(ponto_01[0] + ponto_01[1]);
+        ponto_02[0] = Ler.nextInt();
+        ponto_02[1] = Ler.nextInt();
+
+        calcular_distancia(ponto_01, ponto_02);
+
+        //System.out.print(ponto_01[0] + ponto_01[1]);
         
         Ler.close();
+    }
+
+    public void calcular_distancia(P1, P2) {
+        distancia = Math.pow((P1[0] - P2[0]), 2)  ;       //distância = raiz de ((x2-x1)² + (y2 - y1)²) 
+
     }
 }
 
