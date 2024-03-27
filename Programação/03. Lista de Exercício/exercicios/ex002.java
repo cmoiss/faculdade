fbb0b7a48b80eae74f81447d466b4ddb1922065e
Caio Moisés Souza Barbalho <ocaiomoises@gmail.com>
2024-03-27 00:20:56 -0300

    estrutura básica

---
+++ Programação/03. Lista de Exercício/exercicios/ex002.java
@@ -0,0 +1,23 @@
+//package exercicios;
+
+import java.util.Scanner;
+//import exercicios.ex001;
+
+public class ex002 {
+    public static void main(String[] args){
+        int a, b, c, r, s, d;
+        Scanner Read = new Scanner(System.in);
+
+        System.out.print("Digite o valor de A: ");
+        a = Read.nextInt();
+
+        //ex001.espaço_branco();
+        System.out.print("Digite o valor de B: ");
+        b = Read.nextInt();
+
+        System.out.print("Digite o valor de C: ");
+        c = Read.nextInt();
+
+
+    }
+}
--- Programação/03. Lista de Exercício/exercícios/ex002.java
+++
@@ -1,5 +0,0 @@
-public class ex002 {
-    public static void main(String[] args){
-        
-    }
-}
