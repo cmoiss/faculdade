ef7726837baf3e18b9eaa8068c7d444f9186c71a
Caio Moisés Souza Barbalho <ocaiomoises@gmail.com>
2024-03-27 00:34:38 -0300

    create methods R&S

--- Programação/03. Lista de Exercício/exercicios/ex002.java
+++ Programação/03. Lista de Exercício/exercicios/ex002.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class ex002 {
     public static void main(String[] args){
-        int a, b, c, r, s, d;
+        int a, b, c, d;
         Scanner Read = new Scanner(System.in);
 
         do {
@@ -24,5 +24,20 @@ public class ex002 {
             System.out.print("Digite o valor de C, sendo ele um número positivo (>0): ");
             c = Read.nextInt();
         } while (c < 1);
+
+        calculo_R(a, b);
+        calculo_S(b, c);
+    }
+
+    public static int calculo_R(int a, int b){
+        //R = (A + B)²
+        double r = Math.pow((a + b), 2);
+        return (int) r;
+    }
+
+    public static int calculo_S(int b, int c){
+        //S = (B + C)²
+        double s = Math.pow((b + c), 2);
+        return (int) s;
     }
 }
