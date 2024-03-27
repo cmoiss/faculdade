fd8bf62ad7c1f2a3637233f94a5eb804dc6c0f1e
Caio Moisés Souza Barbalho <ocaiomoises@gmail.com>
2024-03-26 23:53:46 -0300

    Código Finalizado

--- Programação/03. Lista de Exercício/exercícios/ex001.java
+++ Programação/03. Lista de Exercício/exercícios/ex001.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class ex001 {
     public static void main(String[] args) {
         Scanner Ler = new Scanner(System.in);
-        int x1, y1, x2, y2, distancia;
-        int ponto_01[] = new int[2];
-        int ponto_02[] = new int[2];
+    
+        int[] ponto_01 = new int[2];
+        int[] ponto_02 = new int[2];
         
         System.out.println("Digite as coordenadas do ponto 1 (x1, y1)");
         //x1 = Ler.nextInt();
@@ -14,6 +14,7 @@ public class ex001 {
         ponto_01[0] = Ler.nextInt();
         ponto_01[1] = Ler.nextInt();
         
+        espaço_branco();
         System.out.println("Ótimo, agora digite as coordenadas do ponto 2 (x2, y2)");
         //x2 = ponto_01[0];
         //y2 = ponto_01[1];
@@ -21,6 +22,7 @@ public class ex001 {
         ponto_02[0] = Ler.nextInt();
         ponto_02[1] = Ler.nextInt();
 
+        espaço_branco();
         calcular_distancia(ponto_01, ponto_02);
 
         //System.out.print(ponto_01[0] + ponto_01[1]);
@@ -28,9 +30,23 @@ public class ex001 {
         Ler.close();
     }
 
-    public void calcular_distancia(P1, P2) {
-        distancia = Math.pow((P1[0] - P2[0]), 2)  ;       //distância = raiz de ((x2-x1)² + (y2 - y1)²) 
+    public static void calcular_distancia(int[] P1, int[] P2) {
+        //distância = raiz de ((x2-x1)² + (y2 - y1)²) 
+        double x, y, distancia;
+        
+        x = P2[0] - P1[0];
+        x =  Math.pow(x, 2);
 
+        y = P2[1] - P1[1];
+        y = Math.pow(y, 2);
+
+        distancia = Math.sqrt(x + y);
+
+        System.out.print("A distância entre o ponto 1 e o ponto 2, é: " + distancia);
+    }
+
+    public static void espaço_branco(){
+        System.out.print("");
     }
 }
 
