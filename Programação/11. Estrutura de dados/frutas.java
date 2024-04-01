55d7741a90633cd6d6c7a6c88bf63b5c3ab6b3ed
Caio Moisés Souza Barbalho <160979584+cmoiss@users.noreply.github.com>
2024-04-01 10:29:13 -0300

    criei ambos exercícios

--- Programação/11. Estrutura de dados/frutas.java
+++ Programação/11. Estrutura de dados/frutas.java
@@ -2,9 +2,17 @@
 
 public class frutas {
     public static void main(String[] args) {
+        String[] frutas = new String[5];
+
+        frutas[0] = "Gomu Gomu no Mi";
+        frutas[1] = "Bara Bara no Mi";
+        frutas[2] = "Mera Mera no Mi";
+        frutas[3] = "Gura Gura no Mi";
+        frutas[4] = "Yami Yami no Mi";
+
+
         for(int i = 0; i < 5; i++){
-            String[] frutas = new String[5];
-            
+            System.out.println(frutas[i]);
         }        
     }
 }
