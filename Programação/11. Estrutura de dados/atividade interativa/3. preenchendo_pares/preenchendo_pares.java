1c68e156daca16df44b9b47b78b51d42424ff982
Caio Moisés Souza Barbalho <ocaiomoises@gmail.com>
2024-04-02 05:25:21 -0300

    Create preenchendo_pares.java

---
+++ Programação/11. Estrutura de dados/atividade interativa/3. preenchendo_pares/preenchendo_pares.java
@@ -0,0 +1,11 @@
+public class preenchendo_pares {
+    public static void main(String[] args) {
+        int[] array = new int[20];
+        
+        for(int i = 0; i < 20; i++) {
+            array[i] = i;
+            
+            System.out.println(array);
+        }
+    }
+}
