def0dfcc883214a0697374d7b016e196b454f666
Caio Moisés Souza Barbalho <ocaiomoises@gmail.com>
2024-04-02 06:02:11 -0300

    preenchendo_pares is done && verificar_vetores is created

--- Programação/11. Estrutura de dados/atividade interativa/3. preenchendo_pares/preenchendo_pares.java
+++ Programação/11. Estrutura de dados/atividade interativa/3. preenchendo_pares/preenchendo_pares.java
@@ -1,11 +1,12 @@
 public class preenchendo_pares {
     public static void main(String[] args) {
-        int[] array = new int[20];
+        int[] array = new int[21];
         
-        for(int i = 0; i < 20; i++) {
-            array[i] = i;
-            
-            System.out.println(array);
+        for(int i = 0; i <= array.length; i++) {
+            if(i % 2 == 0) {
+                array[i] = i;
+                System.out.println(array[i]);
+            }  
         }
     }
 }
