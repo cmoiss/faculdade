fd1e950de3d3cba14ec316728eb10f7869f91cd9
Caio <ocaiomoises@gmail.com>
2024-04-05 23:03:27 -0300

    ex008 está quase pronto

--- Programação/03. Lista de Exercício/exercicios/ex008.java
+++ Programação/03. Lista de Exercício/exercicios/ex008.java
@@ -7,21 +7,34 @@ public class ex008 {
         String[] coeficientes = {"a", "b", "c", "d", "e", "f"};
         Scanner Ler = new Scanner(System.in);
 
+        int a = valorCoeficientes[0];
+        int b = valorCoeficientes[1];
+        int c = valorCoeficientes[2];
+        int d = valorCoeficientes[3];
+        int e = valorCoeficientes[4];
+        int f = valorCoeficientes[5];
+        
         System.out.println("Digite os coeficientes");
         
+        do {
         for(int i = 0; i < coeficientes.length; i++) {
             System.out.print(coeficientes[i] + ": ");
             valorCoeficientes[i] = Ler.nextInt();
         }
 
-/*
-        x = ((c * e) - (b * f)) / ((a * e) - (b * d));
-        y = ((a * f) - (c * d)) / ((a * e) - (b * d));
+        if ((a * e) - (b * d) != 0) { // evita o erro: Exception in thread "main" java.lang.ArithmeticException: / by zero at ex008.main(ex008.java:24)
+            x = ((c * e) - (b * f)) / ((a * e) - (b * d));
+            y = ((a * f) - (c * d)) / ((a * e) - (b * d));
 
-        System.out.print("O resultado de x é: " + x);
-        System.out.print("E o resultado de y é: " + y);
+            System.out.print("O resultado de x é: " + x);
+            System.out.print("E o resultado de y é: " + y);
+        } else {
+            System.out.println("");
+            System.out.println("Erro! O resultado da conta (a * e) - (b * d) resulta em zero.");
+            System.out.println("Por favor digite novamente.");
+        }
+        } while((a * e) - (b * d) == 0);
 
         Ler.close();
-*/
     }
 }
