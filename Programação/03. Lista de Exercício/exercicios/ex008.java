29a18d9975fa84e22d87f06a63f695020b81c1ec
Caio <ocaiomoises@gmail.com>
2024-04-05 22:39:24 -0300

    fiz o input dos coeficientes

--- Programação/03. Lista de Exercício/exercicios/ex008.java
+++ Programação/03. Lista de Exercício/exercicios/ex008.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ex008 {
     public static void main(String[] args) {
+        float x, y;
+        int[] valorCoeficientes = new int[6]; //valor dos coeficientes
+        String[] coeficientes = {"a", "b", "c", "d", "e", "f"};
+        Scanner Ler = new Scanner(System.in);
+
+        System.out.println("Digite os coeficientes");
         
+        for(int i = 0; i < coeficientes.length; i++) {
+            System.out.print(coeficientes[i] + ": ");
+            valorCoeficientes[i] = Ler.nextInt();
+        }
+
+/*
+        x = ((c * e) - (b * f)) / ((a * e) - (b * d));
+        y = ((a * f) - (c * d)) / ((a * e) - (b * d));
+
+        System.out.print("O resultado de x é: " + x);
+        System.out.print("E o resultado de y é: " + y);
+
+        Ler.close();
+*/
     }
 }
