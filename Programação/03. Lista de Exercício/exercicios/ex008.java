e223f4303e0c375ecc5a63ae6b1bc87c972a121c
Caio <ocaiomoises@gmail.com>
2024-04-06 00:08:46 -0300

    ex008 is done

--- Programação/03. Lista de Exercício/exercicios/ex008.java
+++ Programação/03. Lista de Exercício/exercicios/ex008.java
@@ -6,6 +6,14 @@ public class ex008 {
         int[] valorCoeficientes = new int[6]; //valor dos coeficientes
         String[] coeficientes = {"a", "b", "c", "d", "e", "f"};
         Scanner Ler = new Scanner(System.in);
+        
+        espaço_vazio();
+        System.out.println("Digite os coeficientes");
+        
+        for(int i = 0; i < coeficientes.length; i++) {
+            System.out.print(coeficientes[i] + ": ");
+            valorCoeficientes[i] = Ler.nextInt();
+        }
 
         int a = valorCoeficientes[0];
         int b = valorCoeficientes[1];
@@ -13,28 +21,35 @@ public class ex008 {
         int d = valorCoeficientes[3];
         int e = valorCoeficientes[4];
         int f = valorCoeficientes[5];
-        
-        System.out.println("Digite os coeficientes");
-        
-        do {
-        for(int i = 0; i < coeficientes.length; i++) {
-            System.out.print(coeficientes[i] + ": ");
-            valorCoeficientes[i] = Ler.nextInt();
-        }
 
+        /* verificar se os valores estão corretos
+        espaço_vazio();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        */
+        
         if ((a * e) - (b * d) != 0) { // evita o erro: Exception in thread "main" java.lang.ArithmeticException: / by zero at ex008.main(ex008.java:24)
             x = ((c * e) - (b * f)) / ((a * e) - (b * d));
             y = ((a * f) - (c * d)) / ((a * e) - (b * d));
 
-            System.out.print("O resultado de x é: " + x);
-            System.out.print("E o resultado de y é: " + y);
+            espaço_vazio();
+            System.out.println("O resultado de x é: " + x);
+            System.out.println("E o resultado de y é: " + y);
+            espaço_vazio();
         } else {
-            System.out.println("");
+            espaço_vazio();
             System.out.println("Erro! O resultado da conta (a * e) - (b * d) resulta em zero.");
             System.out.println("Por favor digite novamente.");
         }
-        } while((a * e) - (b * d) == 0);
 
         Ler.close();
+    }
+
+    public static void espaço_vazio() {
+        System.out.println("");
     }
 }
