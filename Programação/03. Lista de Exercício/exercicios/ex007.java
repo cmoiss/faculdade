d9a0c31284d9bce29fbcda3ff13660d630db722d
Caio <ocaiomoises@gmail.com>
2024-04-05 22:14:03 -0300

    ex007 is done

--- Programação/03. Lista de Exercício/exercicios/ex007.java
+++ Programação/03. Lista de Exercício/exercicios/ex007.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class ex007 {
     public static void main(String[] args) {
-        
+        double custoFabrica, percentagemDistribuidor, impostos, custoConsumidor;
+        Scanner Ler = new Scanner(System.in);
+
+        System.err.print("Digite o custo da fábrica: ");
+        custoFabrica = Ler.nextDouble();
+
+        percentagemDistribuidor = custoFabrica * 0.28;
+        impostos = custoFabrica * 0.45;
+        custoConsumidor = custoFabrica + percentagemDistribuidor + impostos;
+
+        System.out.println("O custo ao consumidor final é: " + custoConsumidor);
+        Ler.close();
     }
 }
