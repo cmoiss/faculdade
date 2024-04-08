629c746d3c1be41a5a77f07e86c0e1fdb5955d0a
Caio <ocaiomoises@gmail.com>
2024-04-08 06:01:26 -0300

    media_idade is done

--- Programação/10. Estruturas de Repetição/atividade_interativa/2. Media de Idades/media_idade.java
+++ Programação/10. Estruturas de Repetição/atividade_interativa/2. Media de Idades/media_idade.java
@@ -1,45 +1,37 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class media_idade {
     public static void main(String[] args) {
-        Scanner Read = new Scanner(System.in);
-        int age;
-        List <Integer> numPeople = new ArrayList<>(); //quantidade de pessoas  
-        int listSize; //tamanho da lista
+        int age, listSize = 0, med; //tamanho da lista //média entre idades
         int sumAge = 0; //armazena a soma entre idades
-        int med; //média entre idades
+        int[] numPeople; //quantidade de pessoas  
+        Scanner Read = new Scanner(System.in);
 
+        void_space();
+        System.out.print("Digite a quantidade de pessoas na lista: ");
+        listSize = Read.nextInt();
 
-        do {
-            System.out.print("Digite a idade: ");
+        numPeople = new int[listSize];
+
+        for(int i = 0; i < listSize; i++) {
+            System.out.print("Digite a idade da pessoa "+ (i + 1) + ": ");
             age = Read.nextInt();
 
             if(age >= 0) {
                 sumAge = sumAge + age;
             }
-
-            numPeople.add(age);
-            listSize = numPeople.size();
-
-
-
-            if (age < 0) {
-                listSize = listSize - 1;
-            } else if(listSize == 1) {
-                System.out.println("Lista atualizada, agora a lista possui " + listSize + " item. ");
-                System.out.println(" ");
-
-            } else {
-                System.out.println("Lista atualizada, agora a lista possui " + listSize + " itens. ");
-                System.out.println(" ");
-            }
-        } while(age >= 0);
-        
+        }
+                
         med = sumAge / listSize;
 
-        System.out.print("A média entre essas idades é: " + med);
+        void_space();
+        System.out.println("A média entre essas idades é: " + med);
+        void_space();
+        
         Read.close();
+    }
+
+    public static void void_space() {
+        System.out.println("");
     }
 }
