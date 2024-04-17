1ccb3492abcabe78f51e84e92e90be04f3460315
Caio <ocaiomoises@gmail.com>
2024-04-17 17:22:20 -0300

    Create 'ex012' (Zoo with POO)

---
+++ Programação/12. POO Zoológico/Ave.java
@@ -0,0 +1,8 @@
+public class Ave extends Animal{
+    String Cor_Penas;
+    double Tamanho_Penas;
+    
+    public static void Botar_Ovo() {
+
+    }
+}
---
+++ Programação/12. POO Zoológico/Repteis.java
@@ -0,0 +1,9 @@
+public class Repteis extends Animal{
+    String Tipo_Escama;
+    double Comprimento_Calda;
+
+    private static void Rastejar() {
+    
+    }
+}
+
