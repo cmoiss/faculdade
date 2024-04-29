0292ffd3000ad5d70ba19e0d5a2c965998587ab8
Caio <ocaiomoises@gmail.com>
2024-04-29 13:12:17 +0000

    Create calcularArea()

--- Programação/16. Retangulo/CalculoRetangulo/Calculo.java
+++ Programação/16. Retangulo/CalculoRetangulo/Calculo.java
@@ -1,4 +1,10 @@
 package CalculoRetangulo;
 public class Calculo extends Retangulo {
+    private void calcularArea(float largura, float altura) {
+        this.largura = largura;
+        this.altura = altura;
+        float area = largura * altura;
 
+        System.out.print("A área desse retângulo é: " + area);   
+    }
 }
