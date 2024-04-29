76189159c6e40641b381a7a137e7870d484fae5f
Caio <ocaiomoises@gmail.com>
2024-04-29 14:29:30 +0000

    coringuei fdskkkkkkkkkkk

--- Programação/16. Retangulo/CalculoRetangulo/Calculo.java
+++ Programação/16. Retangulo/CalculoRetangulo/Calculo.java
@@ -1,26 +1,28 @@
 package CalculoRetangulo;
 public class Calculo extends Retangulo {
-    private void calcularArea(float largura, float altura) {
+    private float calcularArea(float largura, float altura) {
         this.largura = largura;
         this.altura = altura;
         float area = largura * altura;
 
-        System.out.print("A área desse retângulo é: " + area);   
+        return area;
     }
 
-    private void calcularPerimetro(float largura, float altura) {
+    private float calcularPerimetro(float largura, float altura) {
         this.largura = largura;
         this.altura = altura;
         float perimetro = largura * altura;    
 
-        System.out.print("O perímetro desse retângulo é: " + perimetro);   
+        return perimetro;
     }
 
     public void getCalcularArea(float largura, float altura) {
-        calcularArea(largura, altura);
+        float area = calcularArea(largura, altura);
+        System.out.println("A área desse retângulo é: " + area);
     }
 
     public void getCalcularPerimetro(float largura, float altura) {
-        calcularPerimetro(largura, altura);
+        float perimetro = calcularPerimetro(largura, altura);
+        System.out.println("O perímetro desse retângulo é: " + perimetro);
     }
 }
--- Programação/16. Retangulo/CalculoRetangulo/Retangulo.java
+++ Programação/16. Retangulo/CalculoRetangulo/Retangulo.java
@@ -1,9 +1,33 @@
 package CalculoRetangulo;
+import java.util.Scanner;
+
 public class Retangulo {
     public static void main(String[] args) {
         float largura;
         float altura;
+        Scanner ler = new Scanner(System.in);
+        Calculo calculo = new Calculo();
 
+        System.out.print("Digite a largura do retângulo: ");
+        largura = ler.nextFloat();
         
+        System.out.print("Digite a altura do retângulo: ");
+        altura = ler.nextFloat();
+
+        calculo.getCalcularArea(largura, altura);
+        calculo.getCalcularPerimetro(largura, altura);
+
+        /*
+        System.out.print("A área desse retângulo é: " + calculo.getCalcularArea(largura, altura));   
+        System.out.print("O perímetro desse retângulo é: " + calculo.getCalcularArea(largura, altura)); 
+        */
+
+        /*
+        System.out.println(Calculo.getCalcularArea(largura, altura));
+        System.out.println(Calculo.getCalcularPerimetro(largura, altura));
+        */
+
+
+        ler.close();
     }
 }
